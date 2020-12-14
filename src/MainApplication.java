1a377885a34be0f8d7c855cb169a7cb99e5bfcfa
Prosper Ekwerike <pekwerike@gmail.com>
2020-12-14 19:56:23 +0100

    Code refactor

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -5,16 +5,39 @@ import java.net.Socket;
 public class MainApplication {
 
     public static void main(String[] args) throws IOException {
-        Socket server = new Socket("192.168.43.190", 8085);
-        System.out.println("Connected to server");
+        Runnable runnable = () -> {
+            Socket server = null;
+            try {
+                server = new Socket("192.168.43.190", 8085);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Connected to server");
 
-        VideosMovement videosMovement = new VideosMovement();
-        videosMovement.receiveVideo(server);
+            VideosMovement videosMovement = new VideosMovement();
+            // videosMovement.receiveVideo(server);
+
+            File video1 = getVideoFile("148 Introduction to the DOM");
+            File video2 = getVideoFile("149 Defining the DOM");
+            File video3 = getVideoFile("157 Manipulating Attributes");
+            File video4 = getVideoFile("152 Important Selector Methods");
+            File[] videoCollection = { video2, video3, video4};
+
+            try {
+               // videosMovement.transferVideo(videoCollection,server);
+                videosMovement.receiveVideo(server);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
 
     }
 
 
-    private static File createFile() {
-        return new File("C:\\Users\\Prosper's PC\\Pictures\\new" + System.currentTimeMillis() + ".jpg");
+    private static File getVideoFile(String name){
+        return new File("C:\\Users\\Prosper's PC\\Desktop\\the web developer bootcamp\\13 DOM Manipulation\\" + name +".mp4");
     }
 }
--- src/VideosMovement.java
+++ src/VideosMovement.java
@@ -32,6 +32,33 @@ public class VideosMovement {
 
     }
 
+    void transferVideo(File[] videoCollection, Socket clientSocket) throws IOException {
+        OutputStream clientSocketOS = clientSocket.getOutputStream();
+        BufferedOutputStream clientSocketBOS = new BufferedOutputStream(clientSocketOS);
+        DataOutputStream clientSocketDOS = new DataOutputStream(clientSocketBOS);
+
+        //write the amount of video to transfer
+        clientSocketDOS.writeInt(videoCollection.length);
+
+        // write the length and name of each video to the clientSocket DataOutputStream
+        for(int i = 0; i < videoCollection.length; i++){
+            clientSocketDOS.writeLong(videoCollection[i].length());
+            clientSocketDOS.writeUTF(videoCollection[i].getName());
+        }
+
+        // write the bytes of each video to the clientSocket DataOutputStream
+        for(int i = 0; i < videoCollection.length; i++){
+            FileInputStream videoFileInputStream = new FileInputStream(videoCollection[i]);
+            byte[] buffer = videoFileInputStream.readAllBytes();
+            clientSocketDOS.write(buffer);
+            videoFileInputStream.close();
+        }
+
+        clientSocketDOS.close();
+        System.out.println("Done");
+    }
+
+
     private static File createFile(String videoName) {
         return new File("C:\\Users\\Prosper's PC\\Pictures\\" + videoName + ".mp4");
     }
